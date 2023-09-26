4185a693b3cf0eeb8f5c4834545a5271bffa89c0
Nikola Luburic <luburic91@gmail.com>
2023-09-26 10:36:58 +0200

    fix: Resolves naming issue.

--- src/Modules/Tours/Explorer.Tours.Tests/Integration/Administration/EquipmentCommandTests.cs
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/Administration/EquipmentCommandTests.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 
-namespace Explorer.Tours.Tests.Integration.Management;
+namespace Explorer.Tours.Tests.Integration.Administration;
 
 [Collection("Sequential")]
 public class EquipmentCommandTests : BaseToursIntegrationTest
